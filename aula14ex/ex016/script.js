2847d8fa6ccad941245b8ccbefa7606779cddf7e
Phelipe Vieira Guimarães <137660442+phelipeguimaraes@users.noreply.github.com>
2024-02-27 18:37:09 -0300

    Mini exercicio contador

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -1,1 +1,32 @@
-alert('Olá!')
+function contar() {
+    let inicio = window.document.getElementById('txti')
+    let fim = window.document.getElementById('txtf')
+    let passo = window.document.getElementById('txtp')
+    let res = window.document.getElementById('res')
+
+    if(inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+      //  window.alert('[ERRO] Faltam dados!')
+        res.innerHTML = 'Impossível contar'
+    } else {
+        res.innerHTML = 'Contando: <br>'
+        let i = Number(inicio.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        if (p <= 0) {
+            window.alert('Passo inválido')
+        }
+
+        if(i < f) {
+            // Contagem crescente
+            for(let c = i; c <= f; c += p) {
+                res.innerHTML += `${c} \u{1F449}`
+            }
+        } else {
+            // Contagem decrescente
+            for(let c = i; c >= f; c-= p) {
+                res.innerHTML += ` ${c} \u{1F449}`
+            }
+        }
+        res.innerHTML += `\u{1F3C1}`
+    }
+}
